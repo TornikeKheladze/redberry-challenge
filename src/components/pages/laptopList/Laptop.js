b141310a186b961c9465fd04a205b695859cac94
Tornike Kheladze <xeladze.tornike@gmail.com>
2022-09-04 16:38:59 +0400

    created laptoplist

--- src/components/pages/laptopList/Laptop.js
+++ src/components/pages/laptopList/Laptop.js
@@ -1,4 +1,22 @@
-const Laptop = () => {
-  return <div>Laptop</div>;
+import { Link } from "react-router-dom";
+
+const Laptop = ({ data }) => {
+  console.log(data.laptop);
+  return (
+    <div className="laptop">
+      <img
+        src={`https://pcfy.redberryinternship.ge${data.laptop.image}`}
+        alt="laptop image"
+      />
+      <div className="listInfo">
+        <p>
+          {data.user.name} {data.user.surname}
+        </p>
+        <p className="second">{data.laptop.name}</p>
+        <Link to={`/laptop/${data.laptop.id}`}>მეტის ნახვა</Link>
+      </div>
+    </div>
+  );
 };
+
 export default Laptop;
--- src/components/pages/laptopList/LaptopList.js
+++ src/components/pages/laptopList/LaptopList.js
@@ -1,6 +1,10 @@
+import "./LaptopList.scss";
 import { useEffect } from "react";
 import { fetchLaptops } from "../../../features/laptopsSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+import leftArrow from "../../../assets/left-button.png";
 import Laptop from "./Laptop";
 const LaptopList = () => {
   const dispatch = useDispatch();
@@ -8,9 +12,21 @@ const LaptopList = () => {
     dispatch(fetchLaptops());
   }, []);
   const laptops = useSelector(({ generals }) => generals.laptops);
-  console.log(laptops);
   const renderList =
     laptops && laptops.map((lap) => <Laptop key={lap.laptop.id} data={lap} />);
-  return <div>{renderList}</div>;
+  return (
+    <div className="laptopList">
+      <div className="listHeader">
+        <button>
+          <Link to="/">
+            <img src={leftArrow} />
+          </Link>
+        </button>
+        <h1>ჩანაწერების სია</h1>
+      </div>
+      <div className="renderList">{renderList}</div>
+    </div>
+  );
 };
+
 export default LaptopList;
